230aa8a74d78849ad6637b02e1cffbc26b45dc74
Thomas Kilgour <thomas@kilgour.co>
2019-07-05 11:12:34 -0400

    fix oc button hover/focus state

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <p class="italic">A complete list of all Reconcillable Difference's Smoiler Smots.</p>
+    <p class="italic mt-2">A complete list of all Reconcillable Difference's Smoiler Smots.</p>
     <div v-for="show in filteredShows" :key="show.id" class="mt-10 px-4 py-5 border border-gray-700 rounded-lg">
       <h2 v-show="show.node.topic" class="text-2xl">{{ show.node.topic || show.node.title }}</h2>
       <div class="mt-2 flex">
@@ -17,7 +17,7 @@
       </div>
       <p class="snippet mt-1" v-html="show.node.snippet" />
       <div class="flex">
-        <a class="mt-8 px-2 py-1 flex justify-center border-2 border-orange-600 bg-white text-orange-700 rounded outline-none hover:bg-orange-800 focus:bg-orange-800" :href="`https://overcast.fm/${show.node.overcastId}/${show.node.timestamps[0]}`">
+        <a class="mt-8 px-2 py-1 flex justify-center border-2 border-orange-600 bg-white text-orange-700 rounded outline-none hover:bg-orange-600 hover:text-white focus:bg-orange-600 focus:text-white" :href="`https://overcast.fm/${show.node.overcastId}/${show.node.timestamps[0]}`">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" width="20" class="mr-2 fill-current">
             <path d="M500 900c-221.67 0-400-178.33-400-400s178.33-400 400-400 400 178.33 400 400-178.33 400-400 400zm0-158.33l30-30-30-115-30 115 30 30zM461.67 740l-15 56.67 35-35-20-21.67zm76.66 0l-20 20 35 35-15-55zm28.34 103.33L500 776.67l-66.67 66.66C455 846.67 476.67 850 500 850s45-1.67 66.67-6.67zM500 150c-193.33 0-350 156.67-350 350 0 153.33 98.33 283.33 235 330l75-278.33C443.33 540 433.33 520 433.33 498.33c0-36.66 30-66.66 66.67-66.66s66.67 30 66.67 66.66c0 21.67-10 40-26.67 53.34L615 830c136.67-46.67 235-176.67 235-330 0-193.33-156.67-350-350-350zm228.33 523.33c-8.33 10-23.33 13.34-35 5C681.67 670 680 655 688.33 645c0 0 45-60 45-145s-45-145-45-145c-8.33-10-6.66-25 5-33.33 11.67-8.34 26.67-5 35 5 3.34 3.33 55 71.66 55 173.33 0 101.67-51.66 170-55 173.33zm-121.66-65c-10-10-8.34-28.33 1.66-38.33 0 0 25-26.67 25-70s-25-68.33-25-70c-10-10-10-26.67-1.66-38.33 10-11.67 25-11.67 35-1.67 1.66 1.67 41.66 43.33 41.66 110s-40 108.33-41.66 110c-10 10-25 8.33-35-1.67zm-213.34 0c-10 10-25 11.67-35 1.67-1.66-3.33-41.66-43.33-41.66-110s40-106.67 41.66-110c10-10 26.67-10 35 1.67 10 10 8.34 28.33-1.66 38.33 0 1.67-25 26.67-25 70s25 70 25 70c10 10 11.66 26.67 1.66 38.33zm-86.66 70c-11.67 8.34-26.67 5-35-5-3.34-3.33-55-71.66-55-173.33 0-101.67 51.66-170 55-173.33 8.33-10 23.33-13.34 35-5C318.33 330 320 345 311.67 355c0 0-45 60-45 145s45 145 45 145c8.33 10 5 25-5 33.33zm0 0"/>
           </svg>
