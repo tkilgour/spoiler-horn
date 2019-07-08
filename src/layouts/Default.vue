711f4db95552b392a397a22d71540dc4e77ca71f
Thomas Kilgour <thomas@kilgour.co>
2019-07-08 14:26:51 -0400

    remove target blank

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,7 +7,7 @@
       <slot/>
     </main>
     <footer class="mt-10 mb-5">
-      <div class="text-sm">Timestamp icon by <a class="text-blue-700 outline-none hover:text-blue-800 focus:text-white focus:bg-blue-800" href="https://www.flaticon.com/authors/lucy-g" title="Lucy G">Lucy G</a> from <a class="text-blue-700 outline-none hover:text-blue-800 focus:text-white focus:bg-blue-800" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a class="text-blue-700 outline-none hover:text-blue-800 focus:text-white focus:bg-blue-800" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+      <div class="text-sm">Timestamp icon by <a class="text-blue-700 outline-none hover:text-blue-800 focus:text-white focus:bg-blue-800" href="https://www.flaticon.com/authors/lucy-g" title="Lucy G">Lucy G</a> from <a class="text-blue-700 outline-none hover:text-blue-800 focus:text-white focus:bg-blue-800" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a class="text-blue-700 outline-none hover:text-blue-800 focus:text-white focus:bg-blue-800" href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
     </footer>
   </div>
 </template>
